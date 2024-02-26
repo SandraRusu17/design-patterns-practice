07401aaabc11974928fb1a12295d6a816ee44b60
SandraRusu <sandra.rusu.ext@extendaretail.com>
2024-02-26 16:10:44 +0200

    Code refactor.

--- src/main/java/patterns/creational/factorymethod/FactoryPatternDemo.java
+++ src/main/java/patterns/creational/factorymethod/FactoryPatternDemo.java
@@ -1,28 +1,30 @@
 package patterns.creational.factorymethod;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FactoryPatternDemo {
 
     public static void main(String[] args) {
-        try {
-            ShapeFactory circleFactory = new CircleFactory();
-            Shape circle = circleFactory.createShape();
-            circle.draw();
 
-            ShapeFactory squareFactory = new SquareFactory();
-            Shape square = squareFactory.createShape();
-            square.draw();
+        List<ShapeFactory> shapeFactoryList = Arrays.asList(
+                new CircleFactory(),
+                new SquareFactory(),
+                new RectangleFactory(),
+                new UnknownShapeFactory()
+        );
 
-            ShapeFactory rectangleFactory = new RectangleFactory();
-            Shape rectangle = rectangleFactory.createShape();
-            rectangle.draw();
+        drawShapes(shapeFactoryList);
+    }
 
-            ShapeFactory unknownShapeFactory = new UnknownShapeFactory();
-            Shape unknownShape = unknownShapeFactory.createShape();
-            unknownShape.draw();
-
-        } catch (UnknownShapeException e) {
-            System.out.println("An exception occurred: " + e.getMessage());
+    private static void drawShapes(List<ShapeFactory> shapeFactoryList) {
+        for (ShapeFactory factory : shapeFactoryList) {
+            try {
+                Shape shape = factory.createShape();
+                shape.draw();
+            } catch (UnknownShapeException e) {
+                System.out.println("An exception occurred: " + e.getMessage());
+            }
         }
-
     }
 }
